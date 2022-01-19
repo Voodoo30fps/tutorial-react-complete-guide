a358fea587173d93a3857cb02216f2bf5860b9dd
Voodoo30fps <voodoo30fps@gmail.com>
2022-01-19 10:40:57 +0200

    organized components into folders and converted to arrow functions

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,10 +1,12 @@
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
 
-import Card from './Card';
+import Card from '../UI/Card';
 
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
@@ -14,6 +16,6 @@ function ExpenseItem(props) {
             </div>
         </Card>
     );
-}
+};
 
 export default ExpenseItem;
