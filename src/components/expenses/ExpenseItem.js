4e38f58e8697aeece7adf91d6153eff5a9e64901
Voodoo30fps <voodoo30fps@gmail.com>
2022-01-27 09:11:57 +0200

    added the filter component

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 
@@ -8,23 +8,23 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    // State is used for data which changes and you want to tell React to re-eveluate it and trigger the component to run again with the new values.
-    // useState() returns: 1) the given variable (props.title) 2) the function to update the variable
-    // destructuring to store the variable and the function to update the variable into title and setTitle (naming convention variablename, setVariablename)
-    const [title, setTitle] = useState(props.title);
+    // // State is used for data which changes and you want to tell React to re-eveluate it and trigger the component to run again with the new values.
+    // // useState() returns: 1) the given variable (props.title) 2) the function to update the variable
+    // // destructuring to store the variable and the function to update the variable into title and setTitle (naming convention variablename, setVariablename)
+    // const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle('Updated');
-    };
+    // const clickHandler = () => {
+    //     setTitle('Updated');
+    // };
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
+            {/* <button onClick={clickHandler}>Change Title</button> */}
         </Card>
     );
 };
